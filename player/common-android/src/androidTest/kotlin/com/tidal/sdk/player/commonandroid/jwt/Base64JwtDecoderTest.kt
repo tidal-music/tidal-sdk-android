77dcb3297a186b3e09f156701770f24513fe5a4a
Kristian Greve Hagen <khagen@squareup.com>
2024-05-07 22:15:53 +0200

    Don't use real values in our jwt tests

--- player/common-android/src/androidTest/kotlin/com/tidal/sdk/player/commonandroid/jwt/Base64JwtDecoderTest.kt
+++ player/common-android/src/androidTest/kotlin/com/tidal/sdk/player/commonandroid/jwt/Base64JwtDecoderTest.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.json.Json
 import org.junit.Test
 
 private const val TOKEN =
-    "eyJraWQiOiJ2OU1GbFhqWSIsImFsZyI6IkVTMjU2In0.eyJ0eXBlIjoibzJfYWNjZXNzIiwidWlkIjoxNjI5MDc1NDAsInNjb3BlIjoid19zdWIgcl91c3Igd191c3IiLCJnVmVyIjowLCJzVmVyIjowLCJjaWQiOjYzNDMsImN1ayI6ImpPSXo2QUhMUWtUSE5Cak9feGFkbWluIiwiZXhwIjoxNzI0ODMyOTY2LCJzaWQiOiIwOTljZjA1OS1mYzI0LTRmYzYtYjVjOS1iMDY4M2RlMjU0MzIiLCJpc3MiOiJodHRwczovL2F1dGgudGlkYWwuY29tL3YxIn0.MPIQUWuQ7I-116oQtLOqqfgIaW01dRw4Ff9zq2iD1K5n2YUjwzfLkpAEsJPPcMoB2jC9q5wdbxZCPBjqetQ30A"
+    "eyJraWQiOiJ2OU1GbFhqWSIsImFsZyI6IkVTMjU2In0.eyJ0eXBlIjoidGVzdCIsInVpZCI6MTIzLCJnVmVyIjowLCJzVmVyIjowLCJjaWQiOjEyMywiY3VrIjoiYWJjIiwiZXhwIjoxMjMsInNpZCI6ImFiZC0xMjMifQ==.MPIQUWuQ7I-116oQtLOqqfgIaW01dRw4Ff9zq2iD1K5n2YUjwzfLkpAEsJPPcMoB2jC9q5wdbxZCPBjqetQ30A"
 
 class Base64JwtDecoderTest {
 
@@ -18,16 +18,14 @@ class Base64JwtDecoderTest {
     @Test
     fun getClaims() {
         val expected = "{\n" +
-            "  \"type\": \"o2_access\",\n" +
-            "  \"uid\": 162907540,\n" +
-            "  \"scope\": \"w_sub r_usr w_usr\",\n" +
+            "  \"type\": \"test\",\n" +
+            "  \"uid\": 123,\n" +
             "  \"gVer\": 0,\n" +
             "  \"sVer\": 0,\n" +
-            "  \"cid\": 6343,\n" +
-            "  \"cuk\": \"jOIz6AHLQkTHNBjO_xadmin\",\n" +
-            "  \"exp\": 1724832966,\n" +
-            "  \"sid\": \"099cf059-fc24-4fc6-b5c9-b0683de25432\",\n" +
-            "  \"iss\": \"https://auth.tidal.com/v1\"\n" +
+            "  \"cid\": 123,\n" +
+            "  \"cuk\": \"abc\",\n" +
+            "  \"exp\": 123,\n" +
+            "  \"sid\": \"abd-123\"\n" +
             "}"
 
         val actualClaim = base64JwtDecoder.getClaims(TOKEN)
