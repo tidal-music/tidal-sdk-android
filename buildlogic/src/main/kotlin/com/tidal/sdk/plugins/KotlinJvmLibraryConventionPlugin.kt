52cfc0b1139861a02bc1c6b59d337ce0b8c89f31
Michael Pohl <mpohl@tidal.com>
2024-05-13 15:12:42 +0200

    Remove configure<JavaPluginExtension> block from Jvm plugin
    gradle-maven-publish plugin already takes care of this, so that this block here now leads to the jar being created twice. This then breaks publishing.
    I've verified that with this block removed the sources jar and IDE code docs are still present as expected

--- buildlogic/src/main/kotlin/com/tidal/sdk/plugins/KotlinJvmLibraryConventionPlugin.kt
+++ buildlogic/src/main/kotlin/com/tidal/sdk/plugins/KotlinJvmLibraryConventionPlugin.kt
@@ -3,8 +3,6 @@ package com.tidal.sdk.plugins
 import com.tidal.sdk.plugins.constant.PluginId
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.kotlin.dsl.configure
 
 internal class KotlinJvmLibraryConventionPlugin : Plugin<Project> {
 
@@ -12,9 +10,6 @@ internal class KotlinJvmLibraryConventionPlugin : Plugin<Project> {
         pluginManager.apply(PluginId.KOTLIN_JVM_PLUGIN_ID)
         ConfiguresDokka()(this)
         ConfiguresKotlinCompiler()(this)
-        configure<JavaPluginExtension> {
-            withSourcesJar()
-        }
         ConfiguresMavenPublish()(this)
         ConfiguresJUnit5()(this)
     }
