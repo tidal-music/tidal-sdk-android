1c879441e6cca211158ff1dd84d97569482e13bb
Kristian Greve Hagen <khagen@tidal.com>
2024-05-21 13:15:05 +0200

    Add support for Fragmented MP4
    This is needed to be able to play those files directly, which is a new way of playing back certain files coming from our backend.

--- player/playback-engine/src/main/kotlin/com/tidal/sdk/player/playbackengine/TidalExtractorsFactory.kt
+++ player/playback-engine/src/main/kotlin/com/tidal/sdk/player/playbackengine/TidalExtractorsFactory.kt
@@ -4,6 +4,7 @@ import androidx.media3.decoder.flac.FlacExtractor
 import androidx.media3.extractor.Extractor
 import androidx.media3.extractor.ExtractorsFactory
 import androidx.media3.extractor.mp3.Mp3Extractor
+import androidx.media3.extractor.mp4.FragmentedMp4Extractor
 import androidx.media3.extractor.mp4.Mp4Extractor
 
 internal class TidalExtractorsFactory : ExtractorsFactory {
@@ -12,6 +13,7 @@ internal class TidalExtractorsFactory : ExtractorsFactory {
         val mp4Extractor = Mp4Extractor()
         val flacExtractor = FlacExtractor()
         val mp3Extractor = Mp3Extractor()
-        return arrayOf(mp4Extractor, flacExtractor, mp3Extractor)
+        val fragmentedMp4Extractor = FragmentedMp4Extractor()
+        return arrayOf(mp4Extractor, flacExtractor, mp3Extractor, fragmentedMp4Extractor)
     }
 }
