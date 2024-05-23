48326f690ba956b5b3260207388c30c51f956e8c
Jorge Antonio Diaz-Benito Soriano <jantonio@tidal.com>
2024-05-23 13:12:17 +0200

    Fix receiver for MockWebServerExtensions functions
    This allows idiomatic usage when creating MockResponses for a custom dispatcher

---
+++ player/testutil/src/main/kotlin/com/tidal/sdk/player/MockResponseExtensions.kt
@@ -0,0 +1,15 @@
+package com.tidal.sdk.player
+
+import okhttp3.mockwebserver.MockResponse
+import okio.Buffer
+import okio.source
+
+fun MockResponse.setBodyFromFile(fileName: String): MockResponse {
+    val buffer = Buffer()
+    javaClass.classLoader.getResourceAsStream(fileName)?.use { inputStream ->
+        buffer.use {
+            it.writeAll(inputStream.source())
+        }
+    }
+    return setBody(buffer)
+}
