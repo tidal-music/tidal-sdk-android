3842ba359e0d31078a9891a3457b005866705f73
Jorge Antonio Diaz-Benito Soriano <jantonio@tidal.com>
2024-05-23 13:23:11 +0200

    Provide DefaultEventReporter with a TestScope in the PlayLog test
    This means refactoring DI to inject a TestScope for DefaultEventReporter so we
    can perform assertions on what we send to EventProducer reliably. Some of the
    new symbols are introduced in other modules, so they have been prefixed to
    indicate that they are there because of the PlayLogTest. Other renames have
    ocurred for consistency with this.
    
    The new DI setup allows providing DefaultEventReporter with a TestScope,
    meaning that  the for-now only test now runs inside a runTest block, and its
    wait for MediaProductEnded switches out the CoroutineContext away from the test
    one into a regular one so that the wait actually happens since we need playback
    to occur for real.

--- player/src/androidTest/kotlin/com/tidal/sdk/player/playlog/PlayLogTest.kt
+++ player/src/androidTest/kotlin/com/tidal/sdk/player/playlog/PlayLogTest.kt
@@ -1,4 +1,4 @@
-package com.tidal.sdk.player
+package com.tidal.sdk.player.playlog
 
 import android.app.Application
 import androidx.test.platform.app.InstrumentationRegistry
@@ -13,18 +13,31 @@ import com.tidal.sdk.auth.model.Credentials
 import com.tidal.sdk.common.TidalMessage
 import com.tidal.sdk.eventproducer.EventSender
 import com.tidal.sdk.eventproducer.model.ConsentCategory
+import com.tidal.sdk.player.Player
 import com.tidal.sdk.player.common.model.MediaProduct
 import com.tidal.sdk.player.common.model.ProductType
+import com.tidal.sdk.player.events.EventReporterModuleRoot
+import com.tidal.sdk.player.events.di.DefaultEventReporterComponent
+import com.tidal.sdk.player.events.playlogtest.PlayLogTestDefaultEventReporterComponentFactory
+import com.tidal.sdk.player.events.reflectionComponentFactoryF
 import com.tidal.sdk.player.playbackengine.model.Event
 import com.tidal.sdk.player.playbackengine.model.Event.MediaProductEnded
 import com.tidal.sdk.player.repeatableflakytest.RepeatableFlakyTest
 import com.tidal.sdk.player.repeatableflakytest.RepeatableFlakyTestRule
+import com.tidal.sdk.player.setBodyFromFile
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
 import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.Dispatcher
@@ -33,14 +46,15 @@ import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.mock
 import org.mockito.kotlin.argThat
 import org.mockito.kotlin.eq
-import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
-class PlayerPlayTest {
+class PlayLogTest {
 
     @get:Rule
     val server = MockWebServer()
@@ -48,16 +62,17 @@ class PlayerPlayTest {
     @get:Rule
     val repeatableFlakyTestRule = RepeatableFlakyTestRule()
 
-    private val gson = Gson()
+    private val eventReporterCoroutineScope =
+        TestScope(StandardTestDispatcher(TestCoroutineScheduler()))
     private val eventSender = mock<EventSender>()
     private lateinit var player: Player
 
     private val dispatcher = object : Dispatcher() {
         override fun dispatch(request: RecordedRequest): MockResponse {
             return if (request.requestUrl?.encodedPath?.startsWith("/v1/tracks") == true) {
-                MockResponse().setBodyFromFileAsText("playbackinfo/tracks/get_1_bts.json")
+                MockResponse().setBodyFromFile("playbackinfo/tracks/playlogtest/get_1_bts.json")
             } else if (request.requestUrl?.encodedPath?.endsWith("test.m4a") == true) {
-                MockResponse().setBodyFromFileAsBuffer("test_5sec.m4a")
+                MockResponse().setBodyFromFile("raw/playlogtest/test_5sec.m4a")
             } else {
                 MockResponse()
             }
@@ -66,7 +81,11 @@ class PlayerPlayTest {
 
     @Before
     fun setUp() {
+        EventReporterModuleRoot.reflectionComponentFactoryF = {
+            PlayLogTestDefaultEventReporterComponentFactory(eventReporterCoroutineScope)
+        }
         server.dispatcher = dispatcher
+
         player = Player(
             InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
                 as Application,
@@ -88,7 +107,7 @@ class PlayerPlayTest {
 
                 override fun isUserLoggedIn() = true
             },
-            eventSender,
+            eventSender = eventSender,
             okHttpClient = OkHttpClient.Builder()
                 .addInterceptor {
                     val request = it.request()
@@ -100,6 +119,18 @@ class PlayerPlayTest {
         )
     }
 
+    companion object {
+        private lateinit var originalEventReporterComponentFactoryF:
+            () -> DefaultEventReporterComponent.Factory
+
+        @BeforeClass
+        @JvmStatic
+        fun beforeAll() {
+            originalEventReporterComponentFactoryF =
+                EventReporterModuleRoot.reflectionComponentFactoryF
+        }
+    }
+
     @After
     fun afterEach() = runBlocking {
         val job = launch { player.playbackEngine.events.first { it is Event.Release } }
@@ -109,22 +140,23 @@ class PlayerPlayTest {
 
     @RepeatableFlakyTest
     @Test
-    fun loadAndPlaySendsCorrectPlaylogEvent() {
+    fun loadAndPlayUntilEnd() = runTest {
         val mediaProduct = MediaProduct(ProductType.TRACK, "1", "TEST", "456")
 
         player.playbackEngine.load(mediaProduct)
         player.playbackEngine.play()
-        runBlocking {
-            withTimeout(8000) {
+        withContext(Dispatchers.Default.limitedParallelism(1)) {
+            withTimeout(8_000) {
                 player.playbackEngine.events.filter { it is MediaProductEnded }.first()
             }
         }
 
+        eventReporterCoroutineScope.advanceUntilIdle()
         verify(eventSender).sendEvent(
             eq("playback_session"),
             eq(ConsentCategory.NECESSARY),
             argThat {
-                with(gson.fromJson(this, JsonObject::class.java)["payload"].asJsonObject) {
+                with(Gson().fromJson(this, JsonObject::class.java)["payload"].asJsonObject) {
                     assertThat(get("startAssetPosition").asDouble).isEqualTo(0.0)
                     assertThat(get("endAssetPosition").asDouble).isBetween(5.0, 5.1)
                     assertThat(get("actualProductId").asString).isEqualTo("1")
