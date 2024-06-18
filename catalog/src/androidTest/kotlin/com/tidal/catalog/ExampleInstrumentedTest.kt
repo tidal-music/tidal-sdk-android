43afabbd251573922a214db91942d3e323598022
Michael Pohl <mpohl@tidal.com>
2024-06-18 14:38:27 +0200

    Create new SDK module Catalog
    Only contains the generated code, plus the added entry to libs.versions.toml

---
+++ catalog/src/androidTest/kotlin/com/tidal/catalog/ExampleInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.tidal.catalog
+
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+internal class ExampleInstrumentedTest {
+
+    @Test
+    fun comparePackageNames() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val instrumentationContext = InstrumentationRegistry.getInstrumentation().context
+        assertEquals(instrumentationContext.packageName, appContext.packageName)
+    }
+}
