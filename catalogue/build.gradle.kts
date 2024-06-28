a5871430973044c5667b951a9217a0317b5a1947
Michael Pohl <mpohl@tidal.com>
2024-06-28 14:49:13 +0200

    Add generated model files

--- catalogue/build.gradle.kts
+++ catalogue/build.gradle.kts
@@ -12,6 +12,11 @@ dependencies {
 
     api(libs.tidal.sdk.common)
 
+    implementation(libs.kotlinxCoroutinesCore)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.loggingInterceptor)
+    implementation(libs.retrofit)
+
     testApi(libs.test.androidx.junit)
     testApi(libs.test.junit5Api)
     testApi(libs.test.junit5Engine)
