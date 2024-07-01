ee21d98728962742a103a3b94bb9c5d5569b0129
Jorge Antonio Diaz-Benito Soriano <jantonio@tidal.com>
2024-07-01 13:54:49 +0200

    Replace argThat with eager failures
    argThat is not descriptive enough for composite conditions.

--- player/src/androidTest/kotlin/com/tidal/sdk/player/playlog/Extensions.kt
+++ player/src/androidTest/kotlin/com/tidal/sdk/player/playlog/Extensions.kt
@@ -2,10 +2,46 @@ package com.tidal.sdk.player.playlog
 
 import assertk.Assert
 import assertk.assertions.isCloseTo
-import kotlin.math.absoluteValue
+import junit.framework.AssertionFailedError
 
 internal fun Assert<Double>.isAssetPositionEqualTo(targetPosition: Double) =
     isCloseTo(targetPosition, 0.5)
 
-internal fun Double.isAssetPositionEqualTo(targetPosition: Double) =
-    (this - targetPosition).absoluteValue < 0.5
+@Suppress("ThrowsCount")
+internal fun <T> Iterable<T>.combinedPassAllOf(
+    vararg checks: Pair<Int, T.() -> Unit>,
+) {
+    val passesLeft = checks.map { it.first }.toMutableList()
+    val throwableMessageMap = mutableMapOf<Int, MutableList<String>>()
+    forEachIndexed { subjectIndex, subject ->
+        var checkIndex = -1
+        while (checkIndex < checks.size) {
+            try {
+                val check = checks[++checkIndex].second
+                subject.check()
+                if (passesLeft[checkIndex] <= 0) {
+                    throw AssertionFailedError(
+                        "Check at index $checkIndex passed more than desired amount of times (${checks[checkIndex].first})" // ktlint-disable max-line-length
+                    )
+                }
+                passesLeft[checkIndex]--
+                throwableMessageMap.remove(subjectIndex)
+                checkIndex = checks.size
+            } catch (throwable: Throwable) {
+                if (!throwableMessageMap.containsKey(subjectIndex)) {
+                    throwableMessageMap[subjectIndex] = mutableListOf()
+                }
+                throwableMessageMap.getValue(subjectIndex).add(throwable.message!!)
+            }
+        }
+    }
+    if (throwableMessageMap.isNotEmpty()) {
+        throw AssertionFailedError("Failed check(s): $throwableMessageMap")
+    }
+    val index = passesLeft.indexOfFirst { it != 0 }
+    if (index != -1) {
+        throw AssertionFailedError(
+            "Missed check: index $index, wanted ${checks[index].first}, missed ${passesLeft[index]}"
+        )
+    }
+}
