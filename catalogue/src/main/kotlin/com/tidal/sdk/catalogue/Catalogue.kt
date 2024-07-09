9cdbbbc1b5c118f760e96caf040a1f0cea8fcdec
Michael Pohl <mpohl@tidal.com>
2024-07-09 10:10:49 +0200

    Add working catalogue client

--- catalogue/src/main/kotlin/com/tidal/sdk/catalogue/Catalogue.kt
+++ catalogue/src/main/kotlin/com/tidal/sdk/catalogue/Catalogue.kt
@@ -1,18 +1,60 @@
 package com.tidal.sdk.catalogue
 
-/**
- * Demo documentation block for [Catalogue]
- * @param optInt The constructor [Catalogue] takes an optional [Int]
- * just to tell you about in this comment.
- * As this is used directly by the demo application, which is using
- * Jetpack Compose, this is a data class,which makes it easily comparable
- * and helps prevent unnecessary recomposition
- */
-data class Catalogue(private val optInt: Int = 5) {
+import com.tidal.sdk.auth.CredentialsProvider
+import com.tidal.sdk.catalogue.generated.apis.AlbumJSONAPI
+import com.tidal.sdk.catalogue.generated.apis.ArtistJSONAPI
+import com.tidal.sdk.catalogue.generated.apis.ProviderJSONAPI
+import com.tidal.sdk.catalogue.generated.apis.TrackJSONAPI
+import com.tidal.sdk.catalogue.generated.apis.VideoJSONAPI
+import com.tidal.sdk.catalogue.networking.RetrofitProvider
+
+class Catalogue(credentialsProvider: CredentialsProvider) {
+
+    private val retrofit by lazy {
+        RetrofitProvider().provideRetrofit(
+            baseUrl,
+            credentialsProvider,
+        )
+    }
+
     /**
-     * This function returns the string "Catalogue! The string is: [optInt]"
+     * Returns an instance of the [AlbumJSONAPI] which can be used
+     * to make API calls to the Album API
      */
-    fun helloCatalogue(): String {
-        return "Catalogue! The int is: $optInt"
+    fun createAlbumAPI(): AlbumJSONAPI {
+        return retrofit.create(AlbumJSONAPI::class.java)
+    }
+
+    /**
+     * Returns an instance of the [ArtistJSONAPI] which can be used
+     * to make API calls to the Artist
+     * API
+     */
+    fun createArtistAPI(): ArtistJSONAPI {
+        return retrofit.create(ArtistJSONAPI::class.java)
+    }
+
+    /**
+     * Returns an instance of the [ProviderJSONAPI] which can be used
+     * to make API calls to the Provider API
+     */
+    fun createProviderAPI(): ProviderJSONAPI {
+        return retrofit.create(ProviderJSONAPI::class.java)
+    }
+
+    /**
+     * Returns an instance of the [TrackJSONAPI] which can be used
+     * to make API calls to the Track API
+     */
+    fun createTrackAPI(): TrackJSONAPI {
+        return retrofit.create(TrackJSONAPI::class.java)
+    }
+
+    /**
+     * Returns an instance of the [VideoJSONAPI] which can be used
+     * to make API calls to the Video API
+     */
+    fun createVideoAPI(): VideoJSONAPI {
+        return retrofit.create(VideoJSONAPI::class.java)
     }
 }
