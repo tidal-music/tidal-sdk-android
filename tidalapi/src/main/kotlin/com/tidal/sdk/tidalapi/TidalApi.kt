aab55aab1d9bed1ba38732547a4513d078476df5
Michael Pohl <mpohl@tidal.com>
2024-10-22 13:18:47 +0200

    Add tidalapi module

---
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/TidalApi.kt
@@ -0,0 +1,19 @@
+package com.tidal.sdk.tidalapi
+
+/**
+ * Demo documentation block for [TidalApi]
+ * @param optInt The constructor [TidalApi] takes an optional [Int]
+ * just to tell you about in this comment.
+ * As this is used directly by the demo application, which is using
+ * Jetpack Compose, this is a data class,which makes it easily comparable
+ * and helps prevent unnecessary recomposition
+ */
+data class TidalApi(private val optInt: Int = 5) {
+
+    /**
+     * This function returns the string "TidalApi! The string is: [optInt]"
+     */
+    fun helloTidalApi(): String {
+        return "TidalApi! The int is: $optInt"
+    }
+}
