345d7d10686e3a33660edfd41d6468bbe7faf9d1
Michael Pohl <mpohl@tidal.com>
2024-10-23 05:30:38 +0200

    Add dependencies needed

--- tidalapi/build.gradle.kts
+++ tidalapi/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.tidal.android.library)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.android.junit5)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 android {
@@ -11,6 +12,15 @@ android {
 dependencies {
 
     api(libs.tidal.sdk.common)
+    api(libs.tidal.sdk.auth)
+
+    api(libs.kotlinx.serialization.json)
+    api(libs.retrofit)
+
+    implementation(libs.kotlinxCoroutinesCore)
+    implementation(libs.okhttp.loggingInterceptor)
+    implementation(libs.converter.kotlinx.serialization)
+    implementation(libs.converter.scalars)
 
     testApi(libs.test.androidx.junit)
     testApi(libs.test.junit5Api)
