d98124c175cff191a67b779a29f8b356305acbc8
Michael Pohl <mpohl@tidal.com>
2024-10-23 07:25:33 +0200

    Add networking classes to provide a configured Retrofit instance

---
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/networking/DefaultAuthenticator.kt
@@ -0,0 +1,24 @@
+package com.tidal.sdk.tidalapi.networking
+
+import com.tidal.sdk.auth.CredentialsProvider
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+/**
+ * Implementation of [Authenticator] that will delegate decision of authenticating to the provided
+ * [CredentialsProvider].
+ * @param[credentialsProvider] A [CredentialsProvider] that provides the access token used in the
+ * Authorization header of the request.
+ */
+internal class DefaultAuthenticator(private val credentialsProvider: CredentialsProvider) :
+    Authenticator {
+
+    override fun authenticate(route: Route?, response: Response): Request? = runBlocking {
+        credentialsProvider.getCredentials()
+    }.successData?.let {
+        response.request.newBuilder().header("Authorization", "Bearer ${it.token}").build()
+    }
+}
