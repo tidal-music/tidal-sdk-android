5352003c2f8b274c207485879a72066a36dd4963
Jorge Antonio Diaz-Benito Soriano <jantonio@tidal.com>
2024-12-05 15:19:15 +0100

    Force dependency version to avoid vulnerability

--- flo/build.gradle.kts
+++ flo/build.gradle.kts
@@ -7,6 +7,21 @@ android {
     namespace = "com.tidal.sdk.flo"
 }
 
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group != "com.google.guava") {
+            return@eachDependency
+        }
+        if (requested.name != "guava") {
+            return@eachDependency
+        }
+        // https://nvd.nist.gov/vuln/detail/cve-2023-2976, comes via moshi-kotlin-codegen 1.15.1
+        if (requested.version == "30.1.1-jre") {
+            useVersion("32.0.1-jre")
+        }
+    }
+}
+
 dependencies {
     ksp(libs.moshi.codegen)
     implementation(libs.moshi)
