b134994683edf65b38ec08aeacd203c311bb9de3
Michael Pohl <mpohl@tidal.com>
2025-05-12 14:19:11 +0200

    Set up and document git-blame-ignore-revs

--- local-setup.sh
+++ local-setup.sh
@@ -8,4 +8,20 @@ create_git_hooks() {
     echo "Done." >&2
 }
 
+setup_git_blame_ignore() {
+    echo ""
+    echo "Would you like to configure Git to ignore marked commits when using git blame?"
+    echo "Commits to be ignored are listed in '.git-blame-ignore-revs' (y/n)"
+    read -r setup_blame_ignore
+
+    if [[ "$setup_blame_ignore" =~ ^[Yy]$ ]]; then
+        echo "Setting up Git blame ignore configuration..."
+        git config blame.ignoreRevsFile .git-blame-ignore-revs
+        echo "Git blame ignore configuration complete."
+    else
+        echo "Skipping Git blame ignore configuration."
+    fi
+}
+
 create_git_hooks
+setup_git_blame_ignore
