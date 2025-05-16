b8fb6b137ba2902b7d1f5161cd6546949603c552
TIDAL Music Tools <svc-github-tidal-music-tools@block.xyz>
2025-05-16 09:23:26 +0000

    Add 32 files via GitHub Actions
    Changed files:\nM  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/ApiClient.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Artists.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Artworks.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Searchresults0.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumsResource.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtistProfileArtRelationshipUpdateOperationPayload.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtistProfileArtRelationshipUpdateOperationPayloadData.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtistsResource.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworkCreateOperationPayload.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworkCreateOperationPayloadData.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworkCreateOperationPayloadDataAttributes.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworkCreateOperationPayloadDataAttributesSourceFile.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworkSourceFile.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtworksAttributes.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/FileStatus.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/FileUploadLink.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/FileUploadLinkMeta.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ImageLink.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/PlaylistsAttributes.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/PlaylistsResource.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchResultsResource.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsAttributes.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsMultiDataDocument.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsMultiDataRelationshipDocument.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsMultiDataRelationshipDocumentIncludedInner.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsRelationships.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsResource.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/SearchresultsSingleDataDocument.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/TracksResource.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/Utils.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/VideoLink.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/VideosResource.kt

--- tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Artworks.kt
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Artworks.kt
@@ -1,5 +1,6 @@
 package com.tidal.sdk.tidalapi.generated.apis
 
+import com.tidal.sdk.tidalapi.generated.models.ArtworkCreateOperationPayload
 import com.tidal.sdk.tidalapi.generated.models.ArtworksMultiDataDocument
 import com.tidal.sdk.tidalapi.generated.models.ArtworksSingleDataDocument
 import retrofit2.Response
@@ -56,5 +57,28 @@ interface Artworks {
     suspend fun artworksIdGet(
         @Path("id") id: kotlin.String,
         @Query("countryCode") countryCode: kotlin.String,
+    ): Response<ArtworksSingleDataDocument>
+
+    /**
+     * Create single artwork. Creates a new artwork. Responses:
+     * - 201:
+     * - 451: Unavailable For Legal Reasons
+     * - 400: Bad request on client party. Ensure the proper HTTP request is sent (query parameters,
+     *   request body, etc.).
+     * - 500: Internal Server Error. Something went wrong on the server party.
+     * - 404: Resource not found. The requested resource is not found.
+     * - 415: Unsupported Media Type. The API is using content negotiation. Ensure the proper media
+     *   type is set into Content-Type header.
+     * - 405: Method not supported. Ensure a proper HTTP method for an HTTP request is used.
+     * - 406: Not acceptable. The server doesn't support any of the requested by client acceptable
+     *   content types.
+     * - 429: Too many HTTP requests have been made within the allowed time.
+     *
+     * @param artworkCreateOperationPayload (optional)
+     * @return [ArtworksSingleDataDocument]
+     */
+    @POST("artworks")
+    suspend fun artworksPost(
+        @Body artworkCreateOperationPayload: ArtworkCreateOperationPayload? = null
     ): Response<ArtworksSingleDataDocument>
 }
--- tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtistsResource.kt
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/ArtistsResource.kt
@@ -38,6 +38,7 @@ data class ArtistsResource(
     SearchResultsMultiDataDocumentIncludedInner,
     ArtistsMultiDataDocumentIncludedInner,
     SearchSuggestionsMultiDataDocumentIncludedInner,
+    SearchresultsMultiDataRelationshipDocumentIncludedInner,
     ArtistsMultiDataRelationshipDocumentIncludedInner,
     PlaylistsMultiDataRelationshipDocumentIncludedInner,
     VideosMultiDataDocumentIncludedInner,
