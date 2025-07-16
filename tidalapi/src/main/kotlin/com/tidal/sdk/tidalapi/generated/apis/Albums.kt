937c78e03107a9fb58a98d293e614c1dca0710d9
TIDAL Music Tools <svc-github-tidal-music-tools@block.xyz>
2025-07-16 08:59:46 +0000

    Update Tidal API - 9 files changed
    Changed files:\nM  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Albums.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayload.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadData.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadDataAttributes.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadDataAttributesCopyright.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadDataRelationships.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadDataRelationshipsArtists.kt
    A  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumCreateOperationPayloadDataRelationshipsArtistsData.kt
    M  tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/models/AlbumsRelationships.kt

--- tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Albums.kt
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/generated/apis/Albums.kt
@@ -1,5 +1,6 @@
 package com.tidal.sdk.tidalapi.generated.apis
 
+import com.tidal.sdk.tidalapi.generated.models.AlbumCreateOperationPayload
 import com.tidal.sdk.tidalapi.generated.models.AlbumsItemsMultiDataRelationshipDocument
 import com.tidal.sdk.tidalapi.generated.models.AlbumsMultiDataDocument
 import com.tidal.sdk.tidalapi.generated.models.AlbumsMultiDataRelationshipDocument
@@ -25,8 +26,11 @@ interface Albums {
      *   it from fulfilling the request
      *
      * @param countryCode ISO 3166-1 alpha-2 country code
+     * @param pageCursor Server-generated cursor value pointing a certain page of items. Optional,
+     *   targets first page if not specified (optional)
      * @param include Allows the client to customize which related resources should be returned.
-     *   Available options: artists, coverArt, items, providers, similarAlbums (optional)
+     *   Available options: artists, coverArt, items, owners, providers, similarAlbums (optional)
+     * @param filterROwnersId User id (optional)
      * @param filterId Album id (optional)
      * @param filterBarcodeId Barcode Id (optional)
      * @return [AlbumsMultiDataDocument]
@@ -34,8 +38,11 @@ interface Albums {
     @GET("albums")
     suspend fun albumsGet(
         @Query("countryCode") countryCode: kotlin.String,
+        @Query("page[cursor]") pageCursor: kotlin.String? = null,
         @Query("include")
         include: @JvmSuppressWildcards kotlin.collections.List<kotlin.String>? = null,
+        @Query("filter[r.owners.id]")
+        filterROwnersId: @JvmSuppressWildcards kotlin.collections.List<kotlin.String>? = null,
         @Query("filter[id]")
         filterId: @JvmSuppressWildcards kotlin.collections.List<kotlin.String>? = null,
         @Query("filter[barcodeId]")
@@ -60,7 +67,7 @@ interface Albums {
      * @param id Album id
      * @param countryCode ISO 3166-1 alpha-2 country code
      * @param include Allows the client to customize which related resources should be returned.
-     *   Available options: artists, coverArt, items, providers, similarAlbums (optional)
+     *   Available options: artists, coverArt, items, owners, providers, similarAlbums (optional)
      * @return [AlbumsSingleDataDocument]
      */
     @GET("albums/{id}")
@@ -169,6 +176,36 @@ interface Albums {
     ): Response<AlbumsItemsMultiDataRelationshipDocument>
 
     /**
+     * Get owners relationship (\&quot;to-many\&quot;). Retrieves owners relationship. Responses:
+     * - 200: Successful response
+     * - 400: Bad request - The request could not be understood by the server due to malformed
+     *   syntax or invalid parameters
+     * - 404: Not found - The requested resource could not be found
+     * - 405: Method not allowed - The request method is not allowed for the requested resource
+     * - 406: Not acceptable - The requested resource is capable of generating only content not
+     *   acceptable according to the Accept headers sent in the request
+     * - 415: Unsupported media type - The request entity has a media type which the server or
+     *   resource does not support
+     * - 429: Too many requests - The user has sent too many requests in a given amount of time
+     * - 500: Internal server error - The server encountered an unexpected condition that prevented
+     *   it from fulfilling the request
+     *
+     * @param id Album id
+     * @param include Allows the client to customize which related resources should be returned.
+     *   Available options: owners (optional)
+     * @param pageCursor Server-generated cursor value pointing a certain page of items. Optional,
+     *   targets first page if not specified (optional)
+     * @return [AlbumsMultiDataRelationshipDocument]
+     */
+    @GET("albums/{id}/relationships/owners")
+    suspend fun albumsIdRelationshipsOwnersGet(
+        @Path("id") id: kotlin.String,
+        @Query("include")
+        include: @JvmSuppressWildcards kotlin.collections.List<kotlin.String>? = null,
+        @Query("page[cursor]") pageCursor: kotlin.String? = null,
+    ): Response<AlbumsMultiDataRelationshipDocument>
+
+    /**
      * Get providers relationship (\&quot;to-many\&quot;). Retrieves providers relationship.
      * Responses:
      * - 200: Successful response
@@ -233,4 +270,27 @@ interface Albums {
         @Query("include")
         include: @JvmSuppressWildcards kotlin.collections.List<kotlin.String>? = null,
     ): Response<AlbumsMultiDataRelationshipDocument>
+
+    /**
+     * Create single album. Creates a new album. Responses:
+     * - 201: Successful response
+     * - 400: Bad request - The request could not be understood by the server due to malformed
+     *   syntax or invalid parameters
+     * - 404: Not found - The requested resource could not be found
+     * - 405: Method not allowed - The request method is not allowed for the requested resource
+     * - 406: Not acceptable - The requested resource is capable of generating only content not
+     *   acceptable according to the Accept headers sent in the request
+     * - 415: Unsupported media type - The request entity has a media type which the server or
+     *   resource does not support
+     * - 429: Too many requests - The user has sent too many requests in a given amount of time
+     * - 500: Internal server error - The server encountered an unexpected condition that prevented
+     *   it from fulfilling the request
+     *
+     * @param albumCreateOperationPayload (optional)
+     * @return [AlbumsSingleDataDocument]
+     */
+    @POST("albums")
+    suspend fun albumsPost(
+        @Body albumCreateOperationPayload: AlbumCreateOperationPayload? = null
+    ): Response<AlbumsSingleDataDocument>
 }
