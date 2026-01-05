1418dcdcab4bfc4df49e3868fe735d30f700701d
Michael Pohl <mpohl@tidal.com>
2026-01-05 08:50:26 +0100

    Test tidalapi module

--- tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/TidalApi.kt
+++ tidalapi/src/main/kotlin/com/tidal/sdk/tidalapi/TidalApi.kt
@@ -7,6 +7,7 @@ package com.tidal.sdk.tidalapi
  *   comment. As this is used directly by the demo application, which is using Jetpack Compose, this
  *   is a data class,which makes it easily comparable and helps prevent unnecessary recomposition
  */
+// Test change for tidalapi module
 data class TidalApi(private val optInt: Int = 5) {
 
     /** This function returns the string "TidalApi! The string is: [optInt]" */
